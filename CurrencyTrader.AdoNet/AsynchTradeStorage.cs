2b8376442ec76d4bad1d39444142ff96bfdeb957
puffboi <spohlmann@css.edu>
2016-11-10 11:39:33 -0600

    ready for assignemnt

---
+++ CurrencyTrader.AdoNet/AsynchTradeStorage.cs
@@ -0,0 +1,17 @@
+﻿using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currencytrader.AdoNet
+{
+    class AsynchTradeStorage : ITradeStorage
+    {
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
